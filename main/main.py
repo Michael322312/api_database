3638a0deead97634c6a35f9b230fe165c6c7d921
Michael322312 <zomisha0808@gmail.com>
2024-01-13 13:05:43 +0200

    Created user model and users list and added logic to work with them

--- main/main.py
+++ main/main.py
@@ -1,18 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+
+users = [
+    User(id=1, username='Michael', email="michaels123@gmail.com"),
+    User(id=2, username="User", email="user23476@gmail.com")
+]
+
+
 @app.get("/users/{user_id}")
 def get_user_by_id(user_id: int):
-    pass
+    for user in users:
+        if user.id == user_id:
+            return user
+    else:
+        return "User not found"
 
 
 @app.get("/users")
 def get_all_users():
-    pass
+    return users
 
 
 @app.post("/create_user")
 def create_user(username: str, email: str):
-    pass
+    user = User(id=users[-1].id, username=username,email=email)
+    users.append(user)
+    return user
